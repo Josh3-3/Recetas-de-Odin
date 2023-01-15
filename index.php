fcb0b94ec91f74b24cc247ff1da94dd564273cae
Josh3-3 <jnmedina21@gmail.com>
2023-01-15 15:42:08 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Recetas De Odin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <h1>Recetas de Odin</h1>
+</body>
+</htm
